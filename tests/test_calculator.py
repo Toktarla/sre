fe9f09d5bf00c15a3281aa56a12d00317e856ca5
unknown <sultantoktar@mail.ru>
2024-06-08 05:15:10 +0500

    Change gitflow

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,10 +3,10 @@ def test_add_positive_numbers():
     assert 1+2 == 3
 
 def test_add_negative_numbers():
-    assert -1 + 1 == -2
+    assert 1 + 1 == -2
 
 def test_add_zero():
     assert 0 + 0 == 0
 
 def test_add_positive_and_negative():
-    assert 5 - 2 == 2
+    assert 4 - 2 == 2
